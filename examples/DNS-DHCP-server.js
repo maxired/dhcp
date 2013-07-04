b2cf1ffeaea0f1fbbc5730770b44d6e97622a295
Maxired <github@maxired.fr>
2013-07-04 10:28:09 +0200

    classical Ip distribution when no names provided add static routing Options

--- examples/DNS-DHCP-server.js
+++ examples/DNS-DHCP-server.js
@@ -1,4 +1,3 @@
-
 var DHCP = {server : require('../lib/server'),util : require('../lib/util')};
 dns = require('dns');
 
@@ -10,68 +9,116 @@ address : "0.0.0.0",
 broadcast : "10.0.0.255",
 dns : "8.8.8.8",
 domainName : "my.domain.net",
+ipRangeStart : "10.0.0.10",
+ipRangeEnd : "10.0.0.25",
 }
  */
 
+var givenIP={};
+
+var extendOptions = function(conf, options){
+  Object.keys(conf.options).forEach(function(key){
+      options[key] = conf.options[key]; });
+  return options;
+};
+
+
+
 
 module.exports = function(){
+
+
+
 
   return {
 
 start :function(conf){
 
          var domainNameRegexp = RegExp( conf.domainName.split('.').join('\\.'));
+
          var getIpFromName = function(name, cb){
            name = domainNameRegexp.test(name) ? name : name+'.'+conf.domainName;  
            dns.resolve4(name, function(err, addresses) {
-               if(err) throw err;
-               cb(err, addresses);
+               cb(err, (addresses && addresses[0]) ? addresses[0]: null);
                } );
          };
 
-         server = new DHCP.server( conf , function(){
+         var getNameFromIp = function(ip, cb){
+           dns.reverse(ip, function(err, domains){
+               if(err) return cb(err);
+               if(!domains || domains.length===0) 
+               return cb(new Error("no name retireved"));
+               console.log(" ip ", ip , "domains", domains);
+               return  cb(err, domains[0]);
+               });
+         }
+
+         var findIpWithoutName = function(cb){
+           DHCP.util.findAvailableIP(conf.ipRangeStart, conf.ipRangeEnd , givenIP, function(err, ip){
+               if(err) return cb(err);             
+               getNameFromIp(ip, function(err2, name){ return cb(err||err2, ip, name)});});
+         };
+         
+        var findIp = function(msg, cb){
+           if(msg.options['12']){
+             return getIpFromName(msg.options['12'], function(err, ip,name){
+                 if(err) {
+                    return findIpWithoutName(cb);
+                 }
+                return cb(err, ip, name)
+                 });
+           }else{
+             return findIpWithoutName(cb);
+           };
+         }
+         
+        server = new DHCP.server( conf , function(){
              server.on("message", function(msg){
 
                });
 
              server.on("request" , function(msg ) { 
-
+               console.log("request");
                //we want to send a DHCPACK
                var options = {
                '53':0x5,
                '1': conf.netmask,
                '28':conf.broadcast,
-               '3': conf.serverIp ,
+               '3': conf.gateway ,
                '15': conf.domainName,
                '6': conf.dns,
-               '12': msg.options['12'],
+               '12': msg.options['12']||"",
                '26': 1500,
                '51':3600,
                0xff : true,
                }
 
-               if(options['12']){
-               getIpFromName(options['12'], function(err, addresses){
+               options = extendOptions( conf, options);
 
-
-
+               findIp( msg , function(err, ip, name){
+                 if(err) console.log(err, msg.options['12']);               
+                 if(name) options['12']=name;
                  msg.respond({
 op:2,
-siaddr : conf.serverIp,
-yiaddr: addresses[0],
+siaddr : conf.nextServer,
+yiaddr: ip,
 flags:msg.flags,
-secs:msg.secs
-}, options);
+secs:msg.secs,
+//sname : conf.sname,
+file : conf.filename,
+}, options, function(err, len){
+givenIP[ip]=true;
+});
 
 
                  } );
-} ;
 
-                 } );
+
+});
 
 
 server.on("discover" , function(msg ) {
-console.log("discover");
+    console.log("discover");
     if(!msg) return; 
 
     var options = {
@@ -81,18 +128,23 @@ console.log("discover");
     '58':36,
     '59':36,
     '1' : conf.netmask,
-    '3': conf.serverIp,
+    '3': conf.gateway,
     0xff : true,
     };
 
-    if(msg.options['12']){
-    getIpFromName(msg.options['12'], function(err, addresses){
+    options = extendOptions( conf, options);
+
+
+    findIp( msg , function(err, ip){
+
       msg.respond({
 op:2,
-siaddr : conf.serverIp,
-yiaddr : addresses[0],
+siaddr : conf.nextServer,
+yiaddr : ip,
 flags : msg.flags,
 secs : msg.secs,
+//sname : conf.sname,
+file : conf.filename,
 
 }, options );
       } );
@@ -100,9 +152,8 @@ secs : msg.secs,
 
 
 
-};
-
 });
+
 
 console.log("listening");
 
--- test/util.js
+++ test/util.js
@@ -1,83 +1,144 @@
-var util = require("../lib/util");
+var util = require("../lib/util"),
+    assert=require('assert');
 
 describe("util", function() {
-  describe(".bufToIp", function() {
-    it("should convert a Buffer to an IPv4 address", function() {
-      util.bufToIp(new Buffer([0x00, 0x00, 0x00, 0x00]))
+    describe(".bufToIp", function() {
+      it("should convert a Buffer to an IPv4 address", function() {
+        util.bufToIp(new Buffer([0x00, 0x00, 0x00, 0x00]))
         .should.equal("0.0.0.0");
 
-      util.bufToIp(new Buffer([0xff, 0xff, 0xff, 0xff]))
+        util.bufToIp(new Buffer([0xff, 0xff, 0xff, 0xff]))
         .should.equal("255.255.255.255");
 
-      util.bufToIp(new Buffer([0xc0, 0xff, 0xee, 0xee]))
+        util.bufToIp(new Buffer([0xc0, 0xff, 0xee, 0xee]))
         .should.equal("192.255.238.238");
-    });
+        });
 
-    it("should convert a Buffer to an IPv4 address starting at the given offset", function() {
-      util.bufToIp(new Buffer([0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff]), 2)
+      it("should convert a Buffer to an IPv4 address starting at the given offset", function() {
+        util.bufToIp(new Buffer([0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff]), 2)
         .should.equal("0.0.0.0");
 
-      util.bufToIp(new Buffer([0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00]), 2)
+        util.bufToIp(new Buffer([0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00]), 2)
         .should.equal("255.255.255.255");
 
-      util.bufToIp(new Buffer([0x00, 0x00, 0xc0, 0xff, 0xee, 0xee, 0x00, 0x00]), 2)
+        util.bufToIp(new Buffer([0x00, 0x00, 0xc0, 0xff, 0xee, 0xee, 0x00, 0x00]), 2)
         .should.equal("192.255.238.238");
-    });
-  });
-
-  describe(".ipToBuf", function() {
-    it("should convert an IPv4 address to a buffer", function() {
-      util.ipToBuf("0.0.0.0")
-        .toString("hex").toLowerCase()
-        .should.equal("00000000");
-
-      util.ipToBuf("255.255.255.255")
-        .toString("hex").toLowerCase()
-        .should.equal("ffffffff");
-
-      util.ipToBuf("192.255.238.238")
-        .toString("hex").toLowerCase()
-        .should.equal("c0ffeeee");
-    });
-  });
-
-  describe(".bufToMac", function() {
-    it("should convert a Buffer to a MAC address", function() {
-      util.bufToMac(new Buffer([0x00, 0x00, 0x00, 0x00, 0x00, 0x00]))
-        .should.equal("00-00-00-00-00-00");
-
-      util.bufToMac(new Buffer([0xff, 0xff, 0xff, 0xff, 0xff, 0xff]))
-        .should.equal("FF-FF-FF-FF-FF-FF");
-
-      util.bufToMac(new Buffer([0xc0, 0xff, 0xee,0xc0, 0xff, 0xee]))
-        .should.equal("C0-FF-EE-C0-FF-EE");
+        });
     });
 
-    it("should convert a Buffer to a MAC address starting at the given offset", function() {
-      util.bufToMac(new Buffer([0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff]), 2)
-        .should.equal("00-00-00-00-00-00");
+    describe(".ipToBuf", function() {
+        it("should convert an IPv4 address to a buffer", function() {
+          util.ipToBuf("0.0.0.0")
+          .toString("hex").toLowerCase()
+          .should.equal("00000000");
 
-      util.bufToMac(new Buffer([0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00]), 2)
-        .should.equal("FF-FF-FF-FF-FF-FF");
+          util.ipToBuf("255.255.255.255")
+          .toString("hex").toLowerCase()
+          .should.equal("ffffffff");
 
-      util.bufToMac(new Buffer([0x00, 0x00, 0xc0, 0xff, 0xee,0xc0, 0xff, 0xee, 0x00, 0x00]), 2)
-        .should.equal("C0-FF-EE-C0-FF-EE");
+          util.ipToBuf("192.255.238.238")
+          .toString("hex").toLowerCase()
+          .should.equal("c0ffeeee");
+          });
+        });
+
+    describe(".bufToMac", function() {
+        it("should convert a Buffer to a MAC address", function() {
+          util.bufToMac(new Buffer([0x00, 0x00, 0x00, 0x00, 0x00, 0x00]))
+          .should.equal("00-00-00-00-00-00");
+
+          util.bufToMac(new Buffer([0xff, 0xff, 0xff, 0xff, 0xff, 0xff]))
+          .should.equal("FF-FF-FF-FF-FF-FF");
+
+          util.bufToMac(new Buffer([0xc0, 0xff, 0xee,0xc0, 0xff, 0xee]))
+          .should.equal("C0-FF-EE-C0-FF-EE");
+          });
+
+        it("should convert a Buffer to a MAC address starting at the given offset", function() {
+          util.bufToMac(new Buffer([0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff]), 2)
+          .should.equal("00-00-00-00-00-00");
+
+          util.bufToMac(new Buffer([0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00]), 2)
+          .should.equal("FF-FF-FF-FF-FF-FF");
+
+          util.bufToMac(new Buffer([0x00, 0x00, 0xc0, 0xff, 0xee,0xc0, 0xff, 0xee, 0x00, 0x00]), 2)
+          .should.equal("C0-FF-EE-C0-FF-EE");
+          });
     });
-  });
 
-  describe(".macToBuf", function() {
-    it("should convert a MAC address to a buffer", function() {
-      util.macToBuf("00-00-00-00-00-00")
-        .toString("hex").toLowerCase()
-        .should.equal("000000000000");
+    describe(".macToBuf", function() {
+        it("should convert a MAC address to a buffer", function() {
+          util.macToBuf("00-00-00-00-00-00")
+          .toString("hex").toLowerCase()
+          .should.equal("000000000000");
 
-      util.macToBuf("FF-FF-FF-FF-FF-FF")
-        .toString("hex").toLowerCase()
-        .should.equal("ffffffffffff");
+          util.macToBuf("FF-FF-FF-FF-FF-FF")
+          .toString("hex").toLowerCase()
+          .should.equal("ffffffffffff");
 
-      util.macToBuf("C0-FF-EE-C0-FF-EE")
-        .toString("hex").toLowerCase()
-        .should.equal("c0ffeec0ffee");
+          util.macToBuf("C0-FF-EE-C0-FF-EE")
+          .toString("hex").toLowerCase()
+          .should.equal("c0ffeec0ffee");
+          });
+        });
+
+    describe(".findAvailableIp", function(){
+        it("should give the first IP in range", function(next) {
+          util.findAvailableIP("0.0.0.1","0.0.0.2", function(err, ip){
+            ip.should.equal("0.0.0.1");
+            next();
+            });
+          });
+        it("should give the first IP in range", function(next) {
+          util.findAvailableIP("0.0.0.1","0.0.0.2",{}, function(err, ip){
+            ip.should.equal("0.0.0.1");
+            next();
+            });
+          });
+        it("should give the first IP in range", function(next) {
+          util.findAvailableIP("0.0.0.1","0.0.0.2",{'0.0.0.1':true}, function(err, ip){
+            assert(ip);
+            ip.should.equal("0.0.0.2");
+            next();
+            });
+          });
+        it("should give the first IP in range", function(next) {
+          util.findAvailableIP("10.0.0.1","10.0.0.5",{'10.0.0.1':true,
+            '10.0.0.2':true,
+            '10.0.0.3':true
+            }, function(err, ip){
+            assert(ip);
+            ip.should.equal("10.0.0.4");
+            next();
+            });
+          });
+        it("should give the first IP in range", function(next) {
+            util.findAvailableIP("10.0.0.1","10.0.0.5",{'10.0.0.1':true,
+              '10.0.0.2':{},
+              '10.0.0.3':true
+              }, function(err, ip){
+              assert(ip);
+              ip.should.equal("10.0.0.4");
+              next();
+              });
+            });
+
+
+        it("should return nyull when no ip in range", function(next) {
+            util.findAvailableIP("10.0.0.1","10.0.0.3",{'10.0.0.1':true,
+              '10.0.0.2':{},
+              '10.0.0.3':true
+              }, function(err, ip){
+              assert.equal(ip, null);
+              next();
+              });
+            });
+        it("should return null when end smaller than start", function(next) {
+            util.findAvailableIP("10.0.0.8","10.0.0.3",{
+              }, function(err, ip){
+              assert.equal(ip, null);
+              next();
+              });
+            });
     });
-  });
 });
